3fb486552e5a3a37da216c22801490d8219c72db
NIKHIL1453-GIT <bnikhil1453@gmail.com>
2025-07-20 11:57:19 +0530

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+console.log("NIKHIL BHATIA");
+let a = 10 
+console.log(a);
+const accountId = 144553
+let accountEmail = "nik@nik.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 10 //not allowed bcoz it is const.
+accountEmail = "nb@nb.com"
+accountPassword = "5555"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
