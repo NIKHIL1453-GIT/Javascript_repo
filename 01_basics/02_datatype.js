bbc59dffe4baa8c3906ca8c0c68b64920c87b9b5
NIKHIL1453-GIT <bnikhil1453@gmail.com>
2025-07-20 16:44:11 +0530

    datatypes finally done

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -6,11 +6,11 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("nikhil")
 
 
-let name = "hitesh"
-let age = 18
+let name = "nikhil"
+let age = 21
 let isLoggedIn = false
 let state;
 
