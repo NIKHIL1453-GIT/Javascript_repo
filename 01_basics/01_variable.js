cf8ef9c6226cf47654704db92a46e10b75c2393b
NIKHIL1453-GIT <bnikhil1453@gmail.com>
2025-07-20 16:17:26 +0530

    variable finally done

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -6,6 +6,8 @@ let accountEmail = "nik@nik.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
+let accountState;
+
 // accountId = 10 //not allowed bcoz it is const.
 accountEmail = "nb@nb.com"
 accountPassword = "5555"
@@ -13,5 +15,8 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
+/*
+Prefer not to use var ...bcoz of issue of functional scope and block scope.
+*/
